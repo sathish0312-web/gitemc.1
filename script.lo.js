71062b2d3a6f8f9aa0cd7e12d8baf32ce2da66d8
sarankumar <saran89400@gmail.com>
2024-11-23 14:37:04 +0530

    script.lo.js file added

---
+++ script.lo.js
@@ -0,0 +1,17 @@
+const container = document.getElementByI('container');
+const registerbtn=document.getElementById('register');
+const loginbtn = document.getElementById('login');{
+
+
+
+
+
+registerbtn.addEventListener( 'click',  =>
+{
+    container.classList.add"(active)"
+};
+loginbtn.addEventListener('click', () => 
+    {
+        
+        container.classList.remove('active')
+});
